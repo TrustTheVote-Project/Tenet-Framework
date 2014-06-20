2b58457ad5b61fdb2e9787c863007ae09df8a4e5
Aleksey Gureiev <spyromus@noizeramp.com>
2014-06-20 14:58:42 +0400

    sorcery migrations

---
+++ db/migrate/20140620104411_sorcery_reset_password.rb
@@ -0,0 +1,8 @@
+class SorceryResetPassword < ActiveRecord::Migration
+  def change
+    add_column :csf_users, :reset_password_token, :string, :default => nil
+    add_column :csf_users, :reset_password_token_expires_at, :datetime, :default => nil
+    add_column :csf_users, :reset_password_email_sent_at, :datetime, :default => nil
+    add_index  :csf_users, :reset_password_token
+  end
+end
