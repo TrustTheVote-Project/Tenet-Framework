eb1b78c718e20060dcd0641fb271ec2788b85830
Aleksey Gureiev <spyromus@noizeramp.com>
2014-06-20 15:13:49 +0400

    added indexes and profile fields to users

--- app/models/csf/account.rb
+++ app/models/csf/account.rb
@@ -1,6 +1,7 @@
 module Csf
   class Account < ActiveRecord::Base
     belongs_to :state
+    has_many   :users, dependent: :destroy
 
     validates :state, presence: true
     validates :name, presence: true
