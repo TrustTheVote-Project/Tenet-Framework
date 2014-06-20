5e62bb5daefbda0c60b5ae03a62532abd0c88131
Aleksey Gureiev <spyromus@noizeramp.com>
2014-06-20 15:03:05 +0400

    added reset pass email notification stub

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -311,7 +311,7 @@ Rails.application.config.sorcery.configure do |config|
     # mailer class. Needed.
     # Default: `nil`
     #
-    # user.reset_password_mailer =
+    user.reset_password_mailer = 'Csf::Notifications'
 
 
     # reset password email method on your mailer class.
