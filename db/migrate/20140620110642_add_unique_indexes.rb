c5e7739373e93d7f7d7847c59919916ff91ed676
Aleksey Gureiev <spyromus@noizeramp.com>
2014-06-20 15:15:28 +0400

    added indexes and profile fields to users

--- db/migrate/20140620110642_add_unique_indexes.rb
+++ db/migrate/20140620110642_add_unique_indexes.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexes < ActiveRecord::Migration
   def change
-    add_index :accounts, :name, unique: true
-    add_index :users, :login, unique: true
+    add_index :csf_accounts, :name, unique: true
+    add_index :csf_users, :login, unique: true
   end
 end
--- db/migrate/20140620111207_add_profile_fields_to_users.rb
+++ db/migrate/20140620111207_add_profile_fields_to_users.rb
@@ -1,6 +1,6 @@
 class AddProfileFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :phone, :string
-    add_column :users, :title, :string
+    add_column :csf_users, :phone, :string
+    add_column :csf_users, :title, :string
   end
 end
