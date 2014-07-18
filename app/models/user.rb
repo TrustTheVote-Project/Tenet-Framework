c387d2c5edc8b89986cf3a48b597d1db3aa679de
Aleksey Gureiev <spyromus@noizeramp.com>
2014-07-18 12:36:02 +0400

    added roles support

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   validates :password, presence: { if: :resetting_password }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :role, presence: { if: ->{ CsfConfig['user_roles'].any? } }
 
   scope :users_only, -> { where(admin: false) }
 
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717101044) do
+ActiveRecord::Schema.define(version: 20140718082858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20140717101044) do
     t.datetime "last_activity_at"
     t.boolean  "password_set",                    default: false, null: false
     t.string   "last_login_from_ip_address"
+    t.string   "role"
   end
 
   add_index "users", ["account_id"], name: "index_users_on_account_id", using: :btree
