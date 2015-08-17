563565510fa9a4f28aac1659492ccb7221a3ccef
Aleksey Gureiev <spyromus@noizeramp.com>
2015-08-17 14:04:51 +0300

    added locale init before each action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
 
   include Concerns::AuthenticationHelpers
 
+  before_filter :set_locale
   before_filter :setup_gon
+
+  def set_locale
+    I18n.locale = session[:locale] if session[:locale]
+  end
 
   def setup_gon
     gon.env = Rails.env
